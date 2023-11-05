487239cb03d9113abd664ec54a35d3cdaf8f0cb4
Rynte <Podreju.dev@gmail.com>
2023-11-06 01:09:06 +0300

    Добавьте файлы проекта.

---
+++ AdmanApiClient/AdmanClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace AdmanApiClient;
+
+public class AdmanClient
+{
+    private readonly HttpClient _client;
+    private readonly string _login, _mdpass;
+
+
+}
---
+++ AdmanApiClient/Intefaces/IAdmanClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdmanApiClient.Intefaces;
+public interface IAdmanClient
+{
+    Task<bool> VpsReboot(string orderID);
+}
